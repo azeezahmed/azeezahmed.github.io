a45d4b9d35ccb9390ed21fc5c6f28a8ff398395d
azeez-infrrd <azizahmed648@gmail.com>
2022-01-20 10:34:16 +0530

    [dev], updated resume and experience to calculate date-date difference

--- src/globalStore.js
+++ src/globalStore.js
@@ -1,7 +1,8 @@
 import { readable } from 'svelte/store';
 
 export const totalExperience = readable(0, (set) => {
-    const joiningYear = new Date('06/22/2015').getFullYear()
-    const currentYear = new Date().getFullYear()
-	set(currentYear - joiningYear);
+    const joiningYear = new Date('06/22/2015')
+    const currentYear = new Date()
+    const experienceInYears = new Date(currentYear - joiningYear).getFullYear() - 1970
+	set(experienceInYears);
 });
